39970531f69557c0fee0dcdb9cf8b510bd6d7fe1
lordZhouJ <384738012@qq.com>
2023-08-17 23:25:29 +0800

    Replace the EDIMG tool in fdimg0at.tek with the dd command

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 rm hello.img
-nasm ipl.asm -o hello.img
+nasm ipl.asm -o ipl.bin 
+nasm haribote.asm  -o harbote.sys
+
+#这里使用dd命令，将两个文件合起来写入img中
+dd if=ipl.bin of=hello.img conv=notrunc
+dd if=harbote.sys of=hello.img seek=512 bs=512 conv=notrunc
+#gcc ipl haribote -o hello.img
 qemu-system-x86_64 hello.img
